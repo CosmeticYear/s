e606cba12526a6082e94a9186ac1952901e4cec3
CosmeticYear <vinzrecamz@gmail.com>
2025-01-03 19:23:14 -0500

    thecrystal.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,69 @@
-game.Players.DontLosePlz.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.wawablaster.Visible = true
+
+--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx
+--wdadawdawawawawawawawawaw
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawdwaaaaaa
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawdsfesef
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx--wdadawd
+--afassssssdddddddddddewigaminesaopewaoijx
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.wawablaster.Visible = true
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.EquinoxScythe.Visible = true
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.Fake.Visible = true
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.Floral.Visible = true
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.Gauss.Visible = true
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.GreenSword.Visible = true
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.Cursed.Visible = true
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.Liberated.Visible = true
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.LightSword.Visible = true
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.MLGSword.Visible = true
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.MidnightScythe.Visible = true
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.NoLegs.Visible = true
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.ObsidianEdge.Visible = true
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.OldSword.Visible = true
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.Shiver.Visible = true
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.Sniper.Visible = true
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.Stopsign.Visible = true
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.VSword.Visible = true
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.Water.Visible = true
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.nuke.Visible = true
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.opAxe.Visible = true
+game.Players.LocalPlayer.PlayerGui.GameGUI.ClassSelection.ScrollingFrame.opBlaster.Visible = true
+
